39624c7fd7d8a60b0c092e6c79120a068af2a66b
José Felisberto <jm.ffelisberto@gmail.com>
2026-01-07 17:25:38 +0000

    new version of td3 looks good

--- training/td3.py
+++ training/td3.py
@@ -20,7 +20,7 @@ from reward_wrappers import LaneCenteringOvertakeReward
 
 # Training configuration
 AGENT_NAME = "td3_agent"
-TOTAL_TIMESTEPS = 400_000
+TOTAL_TIMESTEPS = 200_000
 os.makedirs(AGENT_NAME, exist_ok=True)
 
 print("=" * 70)
